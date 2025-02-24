21e1e5d6a4764afa7e84412b3ce09380561bd38a
Kennedy Sena <kennedysena.dev@gmail.com>
2025-02-23 21:06:04 -0400

    refactor: Changes to pages by adding a new layout.

--- js/script.js
+++ js/script.js
@@ -93,6 +93,17 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+let darkMode = true
+const buttonToggle = document.getElementById("toggle-mode");
 
+buttonToggle.addEventListener("click", (event) => {
+  document.documentElement.classList.toggle("light")
+
+  const mode = darkMode ? "light" : "dark"
+  event.currentTarget.querySelector(
+    "span"
+  ).textContent = `Ativar ${mode} mode!`;
+  darkMode= !darkMode
+})
 
 
